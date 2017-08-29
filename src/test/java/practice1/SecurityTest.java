fb23381b03690b4629eda2092e9ea980de58ccf8
zjx-immersion <jianxin_home@163.com>
2017-08-29 13:03:09 +0800

    [Jason] refactor for this project

--- src/test/java/practice1/SecurityTest.java
+++ src/test/java/practice1/SecurityTest.java
@@ -2,6 +2,7 @@ package practice1;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -14,11 +15,20 @@ import static org.mockito.Mockito.when;
  * Created by jxzhong on 2017/8/28.
  */
 public class SecurityTest {
+
+
+    private Security security;
+    private SecurityChecker checker;
+
+    @Before
+    public void setUp() throws Exception {
+        checker = mock(SecurityChecker.class);
+        security = new Security(checker);
+    }
+
     @Test
     public void should_get_false_when_user_is_null() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(new Permission()).build();
         Permission currentPermission = new Permission();
         User user = null;
@@ -30,8 +40,6 @@ public class SecurityTest {
     @Test
     public void should_get_false_when_permission_is_null() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(new Permission()).build();
         Permission currentPermission = null;
         User user = new User();
@@ -43,8 +51,6 @@ public class SecurityTest {
     @Test
     public void should_get_false_when_permissions_is_empty() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().build();
         Permission currentPermission = new Permission();
         User user = new User();
@@ -57,8 +63,6 @@ public class SecurityTest {
     @Test
     public void should_get_false_when_permissions_not_include_input_permission_and_check_current_permission_false() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().build();
         Permission currentPermission = new Permission();
         User user = new User();
@@ -73,8 +77,6 @@ public class SecurityTest {
     @Test
     public void should_get_true_when_permissions_not_include_input_permission_and_check_current_permission_true() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         Permission currentPermission = new Permission();
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(new Permission()).build();
         User user = new User();
@@ -88,8 +90,6 @@ public class SecurityTest {
 
     @Test
     public void should_get_true_when_permissions_include_input_permission_and_check_current_permission_false() throws Exception {
-        //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
         Security security = new Security(checker);
         Permission currentPermission = new Permission();
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(currentPermission).build();
@@ -105,8 +105,6 @@ public class SecurityTest {
     @Test
     public void should_get_true_when_user_is_admin() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         Permission currentPermission = new Permission();
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(new Permission()).build();
         User user = new User();
@@ -122,8 +120,6 @@ public class SecurityTest {
     @Test
     public void should_get_true_when_user_is_not_admin() throws Exception {
         //Given
-        SecurityChecker checker = mock(SecurityChecker.class);
-        Security security = new Security(checker);
         Permission currentPermission = new Permission();
         ImmutableList<Permission> permissions = ImmutableList.<Permission>builder().add(new Permission()).build();
         User user = new User();
