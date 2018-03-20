8218ba178aa0bb54f247bf5ca400fe2b8d7a7d82
Lizhen Huang <lzhuang@CNlzhuang.local>
2018-03-20 17:18:43 +0800

    modify answer.md and delete unused code
    Signed-off-by: Lizhen Huang <lzhuang@CNlzhuang.local>

--- src/main/java/practice1/SecurityChecker.java
+++ src/main/java/practice1/SecurityChecker.java
@@ -2,12 +2,10 @@ package practice1;
 
 public class SecurityChecker {
     public  boolean checkPermission(User user, Permission permission) {
-        //todo code
         return false;
     }
 
     public boolean isAdmin() {
-        //todo code
         return false;
     }
 }
--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -15,25 +15,4 @@ public class Order {
         this.tax = new BigDecimal(0.1);
     }
 
-//    public BigDecimal calculate() {
-//        BigDecimal subTotal = new BigDecimal(0);
-//
-//        // Total up line items
-//        for (OrderLineItem lineItem : orderLineItemList) {
-//            subTotal = subTotal.add(lineItem.getPrice());
-//        }
-//
-//        // Subtract discounts
-//        for (BigDecimal discount : discounts) {
-//            subTotal = subTotal.subtract(discount);
-//        }
-//
-//        // calculate tax
-//        BigDecimal tax = subTotal.multiply(this.tax);
-//
-//        // calculate GrandTotal
-//        BigDecimal grandTotal = subTotal.add(tax);
-//
-//        return grandTotal;
-//    }
 }
